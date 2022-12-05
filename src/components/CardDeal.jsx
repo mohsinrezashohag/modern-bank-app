492f139f3b5103267b908adda508172a077b37b5
mohsinrezashohag <mohsinrezashohag2@gmail.com>
2022-12-05 07:07:57 +0600

    finishing touch

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { card } from '../assets/index'
+import styles, { layout } from '../styles'
+import Button from './Button';
 
 const CardDeal = () => {
     return (
-        <div>
+        <section className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    Find a better card deal <br className="sm:block hidden" /> in few easy
+                    steps.
+                </h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+                    aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+                </p>
 
-            <h1>Card Deal</h1>
-        </div>
+                <Button styles={`mt-10`} />
+            </div>
+
+            <div className={layout.sectionImg}>
+                <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+            </div>
+        </section>
     );
 };
 
